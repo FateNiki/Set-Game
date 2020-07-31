5f82ca60a97c4feafdb9ee762f97df6053eda31b
Aleksey Nikitin <fateniki@gmail.com>
2020-08-01 02:28:23 +0400

    Отображение и анимации карточек

--- Set Game/Views/MainView.swift
+++ Set Game/Views/MainView.swift
@@ -12,45 +12,63 @@ struct MainView: View {
     @ObservedObject var viewModel: SetGameViewModel
     
     var body: some View {
-        NavigationView {
-            VStack {
-                Grid(viewModel.tableCards) { card in
-                    CardView(card: card).onTapGesture {
-                        withAnimation {
-                            self.viewModel.choose(card: card)
-                        }
-                    }
-                }
-                
-                HStack(alignment: .center) {
-                    Button(action: {
-                        withAnimation {
-                              self.viewModel.pushAdditionCards()
-                        }
-                    }) {
-                        Text("Add 3 cards")
-                    }.disabled(!self.viewModel.allowForPushing)
-                    Spacer()
-                    Text("Score: \(self.viewModel.score)")
-                }
-            }
-            .navigationBarTitle("Set Game", displayMode: .inline)
-            .navigationBarItems(trailing: Button (action: {
-                withAnimation {
-                    self.viewModel.startGame()
-                }
-            }) {
-                Image(systemName: "plus")
-            })
-            .padding()
-            .onAppear {
-                withAnimation(Animation.easeOut.delay(0.5)) {
-                    self.viewModel.startGame()
-                }
+        GeometryReader { geometry in
+            NavigationView {
+                self.body(for: geometry.size)
             }
         }
-
     }
+    
+    @ViewBuilder
+    private func body(for size: CGSize) -> some View {
+        VStack {
+            Grid(self.viewModel.tableCards) { card in
+                CardView(card: card).onTapGesture {
+                    withAnimation {
+                        self.viewModel.choose(card: card)
+                    }
+                }.transition(AnyTransition.offset(self.randomPoint(for: size)))
+            }
+            
+            HStack(alignment: .center) {
+                Button(action: {
+                    withAnimation {
+                          self.viewModel.pushAdditionCards()
+                    }
+                }) {
+                    Text("Add 3 cards")
+                }.disabled(!self.viewModel.allowForPushing)
+                Spacer()
+                Text("Score: \(self.viewModel.score)")
+            }
+        }
+        .navigationBarTitle("Set Game", displayMode: .inline)
+        .navigationBarItems(trailing: Button (action: {
+            withAnimation(Animation.easeInOut(duration: 3)) {
+                self.viewModel.startGame()
+            }
+        }) {
+            Image(systemName: "plus")
+        })
+        .padding()
+        .onAppear {
+            withAnimation(Animation.easeInOut(duration: 3)) {
+                self.viewModel.startGame()
+            }
+        }
+    }
+    
+    private func randomPoint(for size: CGSize) -> CGSize {
+        let width = CGFloat.random(
+            in:  Bool.random() ? ((size.width+minOffset)...(size.width+maxOffset)) : (-maxOffset...(-minOffset))
+        )
+        let height = CGFloat.random(
+            in:  Bool.random() ? ((size.height+minOffset)...(size.height+maxOffset)) : (-maxOffset...(-minOffset))
+        )
+        return CGSize(width: width, height: height)
+    }
+    private let minOffset: CGFloat = 300
+    private let maxOffset: CGFloat = 500
 }
 
 struct MainView_Previews: PreviewProvider {
